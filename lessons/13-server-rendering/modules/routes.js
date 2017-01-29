66e2d9f65c85db91de79a88131330c32d93dd0b2
Sean Pollock <seanpollock3344@gmail.com>
2017-01-28 20:35:15 -0600

    Scaffolding Route tree as a component so it can be required by server and client.:

---
+++ lessons/13-server-rendering/modules/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, InexRoute } from 'react-router'
+import App from './App'
+import About from './Repos'
+import Repo from './Repo'
+import Home from './Home'
+
+module.exports = (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home} />
+    <Route path="/repos" component={Repos}>
+      <Route path="/repos/:userName/:repoName" component={Repo} />
+    </Route>
+    <Route path="/about" component={About} />
+  </Route>
+)ss
